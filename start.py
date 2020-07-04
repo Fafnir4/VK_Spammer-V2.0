86b35f6d7f8de5d54d133aed5fed164807f69d58
Fafnir <47449268+Fafnir4@users.noreply.github.com>
2020-07-05 00:10:09 +0600

    Update start.py

--- start.py
+++ start.py
@@ -43,42 +43,42 @@ if doit == "0":
     print()
     print("Готово.")
     time.sleep(4)
-    os.system("cls")
+    os.system("clear")
     os.system("python3 start.py")
 
 
 if doit == "1":
     print("Выбран спам одного пользывателя.")
     time.sleep(1)
-    os.system("cls")
+    os.system("clear")
     os.system("python3 1spam.py")
 
 if doit == "2":
     print("Выбран спам беседы.")
     time.sleep(1)
-    os.system("cls")
+    os.system("clear")
     os.system("python3 chat.py")
 
 if doit == "3":
     print("Выбран спам рассылка.")
     time.sleep(1)
-    os.system("cls")
+    os.system("clear")
     os.system("python3 invite.py")
 
 if doit == "4":
     print("Выбран ВЗРЫВ!!!.")
     time.sleep(1)
-    os.system("cls")
+    os.system("clear")
     os.system("python3 exp.py")
 
 if doit <= "4":
     print("Не верное действие")
     time.sleep(1)
-    os.system("cls")
+    os.system("clear")
     os.system("python3 spam.py")
 
 if doit >= "0":
     print("Не верное действие")
     time.sleep(1)
-    os.system("cls")
+    os.system("clear")
     os.system("python3 spam.py")
